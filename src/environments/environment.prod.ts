f01f2c9d6f564d4c6f41a6527a47f95fdd61a322
Mayank <mayanksaxena001@gmail.com>
2020-04-17 15:34:56 +0530

    added gh-pages deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-let baseUrl = 'http://localhost:8080/api';
+let baseUrl = 'https://ec2-13-233-90-57.ap-south-1.compute.amazonaws.com/api';
 export const environment = {
   production: false,
   baseUrl: baseUrl
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 // let API_URL =process.env.SERVER_URL;
 // const value = 'localhost';
-let baseUrl = 'http://localhost:8080/api';
+let baseUrl = 'https://ec2-13-233-90-57.ap-south-1.compute.amazonaws.com/api';
 export const environment = {
   production: false,
-  baseUrl:baseUrl
-  // blockchainServiceUrl: value + '/auth',
-  // blockchainContractServiceUrl: value + '/contract',
-  // blockchainSensorUrl: value + '/sensor',
-  // blockchainWalletUrl: value + '/wallet', 
-  // blockchainTokenUrl: value + '/token',
-  // blockchainTradeUrl: value + '/binance'
+  baseUrl
 };
 
 /*
