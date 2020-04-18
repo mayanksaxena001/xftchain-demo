7d93e94cbf82ad4421d93a3d070507fc4ce9f3c1
Mayank <mayanksaxena001@gmail.com>
2020-04-18 21:35:32 +0530

    added sub domain to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-let baseUrl = 'https://ec2-13-233-90-57.ap-south-1.compute.amazonaws.com/api';
+let baseUrl = 'https://api.xftchain.club/api';
 export const environment = {
   production: false,
   baseUrl: baseUrl
